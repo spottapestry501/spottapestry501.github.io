91a381a3043d9a6397310bcacfdee5f4e92d9ab8
Stoutscientist <stoutvlogs@gmail.com>
2022-09-20 18:10:22 -0500

    Update

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,48 +1,65 @@
-let circle = document.querySelector(".circle");
+// VAR SETUP
+let player = document.querySelector(".player");
 let food = document.querySelector(".food");
 let moveBy = 120;
 let foood = 10;
 let water = 20;
 let energy = 100;
-let deaths = 0;
+let deaths;
 let foodCounter = document.getElementById("foodCounter");
+let fullscreenwarning = document.getElementById("fullscreenwarning");
 let waterCounter = document.getElementById("waterCounter");
+let health;
+
+// SET UP CODE
+
+// MENU CODE
+
+
+// FUNCTIONS
+function damage(damagetaken) {
+  let health = health - damagetaken;
+}
+
+// MOVEMENT CODE
 
 window.addEventListener("load", () => {
-  circle.style.position = "absolute";
-  circle.style.left = 0;
-  circle.style.top = 0;
+  player.style.position = "absolute";
+  player.style.left = 0;
+  player.style.top = 0;
 });
 
 window.addEventListener("keydown", (e) => {
   switch (e.key) {
     case "ArrowLeft":
-      circle.style.left = parseInt(circle.style.left) - moveBy + "px";
+      player.style.left = parseInt(player.style.left) - moveBy + "px";
       break;
     case "ArrowRight":
-      circle.style.left = parseInt(circle.style.left) + moveBy + "px";
+      player.style.left = parseInt(player.style.left) + moveBy + "px";
       break;
     case "ArrowUp":
-      circle.style.top = parseInt(circle.style.top) - moveBy + "px";
+      player.style.top = parseInt(player.style.top) - moveBy + "px";
       break;
     case "ArrowDown":
-      circle.style.top = parseInt(circle.style.top) + moveBy + "px";
+      player.style.top = parseInt(player.style.top) + moveBy + "px";
       break;
       case "a":
-        circle.style.left = parseInt(circle.style.left) - moveBy + "px";
+        player.style.left = parseInt(player.style.left) - moveBy + "px";
         break;
       case "d":
-        circle.style.left = parseInt(circle.style.left) + moveBy + "px";
+        player.style.left = parseInt(player.style.left) + moveBy + "px";
         break;
       case "w":
-        circle.style.top = parseInt(circle.style.top) - moveBy + "px";
+        player.style.top = parseInt(player.style.top) - moveBy + "px";
         break;
       case "s":
-        circle.style.top = parseInt(circle.style.top) + moveBy + "px";
+        player.style.top = parseInt(player.style.top) + moveBy + "px";
         break;
       
   }
 });
+
+// HEALTH, STAMINA, AND ENERGY CODE
 foodCounter.innerHTML = "Food: " + foood
 waterCounter.innerHTML = "Water: " + water
 
