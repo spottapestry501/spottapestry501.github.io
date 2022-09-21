a39cecbbcfb5bbcd49d491e7636b87019c920118
Stoutscientist <stoutvlogs@gmail.com>
2022-09-20 20:18:50 -0500

    Added X and Y

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,7 +1,7 @@
 // VAR SETUP
 let player = document.querySelector(".player");
 let food = document.querySelector(".food");
-let moveBy = 120;
+let moveBy = 50;
 let foood = 10;
 let water = 20;
 let energy = 100;
@@ -9,9 +9,11 @@ let deaths;
 let foodCounter = document.getElementById("foodCounter");
 let fullscreenwarning = document.getElementById("fullscreenwarning");
 let waterCounter = document.getElementById("waterCounter");
+let xCounter = document.getElementById("xCounter");
+let yCounter = document.getElementById("yCounter");
 let health;
 
-// SET UP CODE
+// SETUP CODE
 
 // MENU CODE
 
@@ -19,6 +21,12 @@ let health;
 // FUNCTIONS
 function damage(damagetaken) {
   let health = health - damagetaken;
+}
+
+function setcoords() {
+  let position = player.getBoundingClientRect();
+  xCounter.innerHTML = "X: " + position.left / 50
+  yCounter.innerHTML = "Y: " + position.bottom / 50
 }
 
 // MOVEMENT CODE
@@ -33,27 +41,35 @@ window.addEventListener("keydown", (e) => {
   switch (e.key) {
     case "ArrowLeft":
       player.style.left = parseInt(player.style.left) - moveBy + "px";
+      setcoords();
       break;
     case "ArrowRight":
       player.style.left = parseInt(player.style.left) + moveBy + "px";
+      setcoords();
       break;
     case "ArrowUp":
       player.style.top = parseInt(player.style.top) - moveBy + "px";
+      setcoords();
       break;
     case "ArrowDown":
       player.style.top = parseInt(player.style.top) + moveBy + "px";
+      setcoords();
       break;
       case "a":
         player.style.left = parseInt(player.style.left) - moveBy + "px";
+        setcoords();
         break;
       case "d":
         player.style.left = parseInt(player.style.left) + moveBy + "px";
+        setcoords();
         break;
       case "w":
         player.style.top = parseInt(player.style.top) - moveBy + "px";
+        setcoords();
         break;
       case "s":
         player.style.top = parseInt(player.style.top) + moveBy + "px";
+        setcoords();
         break;
       
   }
@@ -62,6 +78,5 @@ window.addEventListener("keydown", (e) => {
 // HEALTH, STAMINA, AND ENERGY CODE
 foodCounter.innerHTML = "Food: " + foood
 waterCounter.innerHTML = "Water: " + water
-
 
 
