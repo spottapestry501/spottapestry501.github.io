42813f1a21f8ba3fdf64f2f5cc8bb3fb5faf9d6f
spottapestry501 <aidanbradly2017@gmail.com>
2022-12-02 13:47:46 -0600

    added Alarm Clock (Work In Progress)

--- Alarm.js
+++ Alarm.js
@@ -1,4 +1,10 @@
-const selectMenu = document.querySelector("select");
+const currentTime = document.querySelector("h1"),
+content = document.querySelector(".content"),
+selectMenu = document.querySelectorAll("select"),
+setAlarmBtn = document.querySelector("button");
+
+let alarmTime, isAlarmSet,
+ringtone = new Audio("./files/ringtone.mp3");
 
 for (let i = 12; i > 0; i--) {
     i = i < 10 ? `0${i}` : i;
@@ -6,14 +12,33 @@ for (let i = 12; i > 0; i--) {
     selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
 }
 
-for (let i = 59; i > 0; i--) {
+for (let i = 59; i >= 0; i--) {
     i = i < 10 ? `0${i}` : i;
     let option = `<option value="${i}">${i}</option>`;
-    selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
+    selectMenu[1].firstElementChild.insertAdjacentHTML("afterend", option);
 }
 
 for (let i = 2; i > 0; i--) {
-    i = i < 10 ? `0${i}` : i;
-    let option = `<option value="${i}">${i}</option>`;
-    selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
+    let ampm = i == 1 ? "AM" : "PM";
+    let option = `<option value="${ampm}">${ampm}</option>`;
+    selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
 }
+
+setInterval(() => {
+    let date = new Date(),
+    h = date.getHours(),
+    m = date.getMinutes(),
+    s = date.getSeconds(),
+    ampm = "AM";
+    if(h >= 12) {
+        h = h - 12;
+        ampm = "PM";
+    }
+    h = h == 0 ? h = 12 : h;
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+    currentTime.innerText = `${h}:${m}:${s} ${ampm}`;
+
+    console.log(`${h}:${m}:${s} ${ampm}`)
+}, 1000);
