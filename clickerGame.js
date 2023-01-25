61c6c3da8bc8ae329686e6b5aa54680bed333bd9
spottapestry501 <aidanbradly2017@gmail.com>
2023-01-24 18:31:43 -0600

    er

--- clickerGame.js
+++ clickerGame.js
@@ -1,4 +1,8 @@
 var score = 0;
+var fancyPizzaScore = 0
+var fancyPizza = document.getElementById("fancyPizza");
+var oldManPizzaScore = 0;
+var oldManPizza = document.getElementById("oldManPizza");
 const scoreDiv = document.getElementById("score");
 const button = document.getElementById("button");
 
@@ -12,10 +16,29 @@ button.addEventListener("click", e => {
 })
 
 function updatecount(){
+    if(score >= 100){
+        score -= 100;
+        oldManPizzaScore++;
+        scoreDiv.innerHTML = "Score: " + score;
+        oldManPizza.innerHTML = "Old man pizza: " + oldManPizzaScore;
         setInterval(() => {
-            score++;
+            score += 0.5;
             scoreDiv.innerHTML = "Score: " + score;
         }, 1000);
+    }
+}
+
+function updatecount2(){
+    if(score >= 500){
+        score -= 500;
+        fancyPizzaScore++;
+        scoreDiv.innerHTML = "Score: " + score;
+        fancyPizza.innerHTML = "Fancy pizza: " + fancyPizzaScore;
+        setInterval(() => {
+            score += 2;
+            scoreDiv.innerHTML = "Score: " + score;
+        }, 1000);
+    }
 }
 
 
