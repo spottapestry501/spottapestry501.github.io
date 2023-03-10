c1b5eb4d22a9f4bcf672903fcdfa5ce1f63082ca
spottapestry501 <aidanbradly2017@gmail.com>
2023-03-10 11:06:31 -0600

    er

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-ctx.fillRect(0, 0, 10, 10);
+const whiteKeys = document.querySelectorAll('.white-key');
+const blackKeys = document.querySelectorAll('.black-key');
+
+function playNote() {
+  const note = this.dataset.note;
+  const audio = new Audio(`notes/${note}.mp3`);
+  audio.play();
+}
+
+whiteKeys.forEach(key => key.addEventListener('click', playNote));
+blackKeys.forEach(key => key.addEventListener('click', playNote));
